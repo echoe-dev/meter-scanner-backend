5e25dd28e7a7ccaf3f32d51aca5d2e397b3733ff
echoe-dev <celesiphelele@gmail.com>
2026-02-11 20:06:22 +0200

    Actually enabled CORS middleware

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ import csv
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def home():
     return {"status": "Server running"}
